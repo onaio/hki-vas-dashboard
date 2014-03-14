190a979b3fe3dad9d589340deb6bb59b26c1fbac
Matt Berg <mlberg@gmail.com>
2014-03-14 11:54:33 +0300

    MB: Changed language for delta

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ var VAS_INDICATORS = {
     vas_6_59: 'VAS (6-59m)',
     vas_6_59_m: 'VAS (6-59m) Male',
     vas_6_59_f: 'VAS (6-59m) Female',
-    admin_pecs_6_59: 'Admin PECs 5-59m',
+    admin_pecs_6_59: 'Administrative Catchment (6-59m)',
     admin_nat_6_59: 'Administrative 6-59m',
-    pecs_admin_delta: 'PECs Administrative Delta',
+    pecs_admin_delta: 'PECs Administrative &Delta;',
     dw_1259: 'Deworming (12-59m)'
 };
 
