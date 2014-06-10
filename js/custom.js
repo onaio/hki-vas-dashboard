4025869a2190b92fcdc63ddebf70c61282c79df2
William Mucheru <willyk99@gmail.com>
2014-06-10 13:35:08 +0300

    WM: Added login form to view map

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+var slider = $("#slider"),
+	value = 0,
+	year = 0,
+	round = 0;
+
+slider.slider({
+	tooltip: 'none',
+	formater: function(value){
+		year = Math.floor(value);
+		round = (Math.round(value)-year)+1;
+		return Math.floor(value) + ' Round '+round;
+	}
+}).on('slide', function(evt){
+	var value = evt.value;
+	var year = Math.round(value);
+	var round = 1;
+	if((year-value) > 0){
+		year = Math.floor(value);
+		round = 2;
+	}
+	setReportParameters(year, round);
+});
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+    
+$(document).ready(function(){
+    console.log("Cookie data: "+document.cookie);
+    //Hide modal if false
+    if(getCookie("intro")==false){
+        $(".modalbox").css("display","none");
+    }
+    else{
+        document.cookie="intro=false";
+    }
+    
+    $(".close-modal").click(function(e){
+        $(".modalbox").fadeOut(200);
+        e.preventDefault();
+    });
+});
