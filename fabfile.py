419fd7917e1de3bb49ee7dca52da1ab6671e6f3d
Ukang'a Dickson <ukanga@gmail.com>
2014-11-13 19:37:46 +0300

    UK: relative resouce urls

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from fabric.api import cd, env, run
+from fabric.api import cd, env, run, sudo
 
 DEPLOYMENTS = {
     'prod': {
@@ -18,12 +18,10 @@ DEPLOYMENTS = {
 
 def check_key_filename(deployment_name):
     if 'key_filename' in DEPLOYMENTS[deployment_name]:
-        at_least_one_path = False
         key_files = []
         for path in DEPLOYMENTS[deployment_name]['key_filename']:
             if os.path.exists(path):
                 key_files.append(path)
-
 
         if not key_files.__len__():
             exit_with_error("Cannot find required permissions file: %s" %
@@ -55,4 +53,6 @@ def deploy(deployment_name, branch='master'):
     setup_env(deployment_name)
     with cd(env.code_src):
         run("git fetch origin")
+        sudo("chown ubuntu:ubuntu -Rv .")
         run("git checkout origin/%s" % branch)
+        sudo("chown www-data:www-data -Rv .")
